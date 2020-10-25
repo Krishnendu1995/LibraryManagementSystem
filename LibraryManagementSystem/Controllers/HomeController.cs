53140edd88af13ad879fba9b0807d087ebd83be5
KRISHNENDU MURALEEDHARAN <krishalp95@gmail.com>
2020-10-25 12:27:18 +0530

    Add project files.

---
+++ LibraryManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult BookDetails()
+        {
+            return View();
+        }
+
+        public IActionResult BookAvail()
+        {
+            return View();
+        }
+
+    }
+}
